6a4b0689ca36ed4e725d492107ff0d6dafee54d6
AlexisLoya <alexloy117@gmail.com>
2025-03-18 22:39:33 -0600

    Add Secction model and enum; update CategoryList to fetch regions

--- src/modules/category/components/CategoryList.tsx
+++ src/modules/category/components/CategoryList.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { CategoryRepository } from '../definitions'
 import CategoryTable from './CategotyTable'
+import { RegionRepository } from '@/modules/region/definitions'
 
 const CategoryList: React.FC = async () => {
   const categories = await new CategoryRepository().getAll()
+  const regions = await new RegionRepository().getAll()
 
   return (
     <>
-      <CategoryTable values={categories} />
+      <CategoryTable values={categories} regions={regions} />
     </>
   )
 }
