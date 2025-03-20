8a2004d0c3e6ec77d58d9d5496d76771c955f1d8
Mauricio Loya <mauloy15@hotmail.com>
2025-03-19 23:01:17 -0600

    Add sign-out functionality and integrate SignOut component in NavBar

---
+++ src/modules/admin-dashboard/components/signout-button.tsx
@@ -0,0 +1,15 @@
+import { handleSignOut } from '../actions/auth-actions'
+
+export function SignOut() {
+  return (
+    <form action={handleSignOut}>
+      <button
+        type="submit"
+        className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+      >
+        🏃‍♂️
+        <div className="hidden md:block">Sign Out</div>
+      </button>
+    </form>
+  )
+}
--- src/modules/admin-dashboard/layout/NavBar.tsx
+++ src/modules/admin-dashboard/layout/NavBar.tsx
@@ -1,40 +1,41 @@
-"use client";
+'use client'
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import React from "react";
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+import { SignOut } from '../components/signout-button'
 
 const NavBar: React.FC = () => {
-  const pathname = usePathname();
+  const pathname = usePathname()
 
   const links = [
-    { name: "Dashboard", href: "/admin", emoji: "📈" },
+    { name: 'Dashboard', href: '/admin', emoji: '📈' },
     {
-      name: "Ordenes",
-      href: "/admin/ordenes",
-      emoji: "📦",
+      name: 'Ordenes',
+      href: '/admin/ordenes',
+      emoji: '📦'
     },
     {
-      name: "Categorias",
-      href: "/admin/categorias",
-      emoji: "📝",
+      name: 'Categorias',
+      href: '/admin/categorias',
+      emoji: '📝'
     },
     {
-      name: "Catalogo",
-      href: "/admin/catalogo",
-      emoji: "👖",
+      name: 'Catalogo',
+      href: '/admin/catalogo',
+      emoji: '👖'
     },
     {
-      name: "Promociones",
-      href: "/admin/promociones",
-      emoji: "🎉",
+      name: 'Promociones',
+      href: '/admin/promociones',
+      emoji: '🎉'
     },
     {
-      name: "Usuarios",
-      href: "/admin/users",
-      emoji: "👥",
-    },
-  ];
+      name: 'Usuarios',
+      href: '/admin/users',
+      emoji: '👥'
+    }
+  ]
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -42,34 +43,34 @@ const NavBar: React.FC = () => {
         href="/admin"
       />
       <div className="text-black flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        {links.map((link) => {
+        {links.map(link => {
           return (
             <Link
               key={link.name}
               href={link.href}
               className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 ${
-                pathname === link.href ? "bg-sky-100 text-blue-600" : ""
+                pathname === link.href ? 'bg-sky-100 text-blue-600' : ''
               }`}
             >
               <span>{link.emoji}</span>
               <p className="hidden md:block">{link.name}</p>
             </Link>
-          );
+          )
         })}
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
-          <button
+
+        <SignOut />
+        {/* <button
             type="button"
             onClick={() => console.log("sign out")}
             className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             🏃‍♂️
             <div className="hidden md:block">Sign Out</div>
-          </button>
-        </form>
+          </button> */}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
