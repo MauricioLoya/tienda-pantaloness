0704cfb4b2a7e4b2477658a0e2dd1260971becff
Mauricio Loya <mauloy15@hotmail.com>
2025-04-06 11:52:33 -0600

    Add ResendEmailService for sending emails and update package dependencies

---
+++ src/services/email/ResendEmailService.ts
@@ -0,0 +1,21 @@
+import { Resend } from 'resend';
+import { EmailPayload, EmailService } from './EmailService';
+
+export class ResendEmailService implements EmailService {
+  private resend: Resend;
+  private sender: string;
+
+  constructor(apiKey: string, sender: string) {
+    this.resend = new Resend(apiKey);
+    this.sender = sender;
+  }
+
+  async sendEmail(payload: EmailPayload): Promise<void> {
+    await this.resend.emails.send({
+      from: this.sender,
+      to: payload.to,
+      subject: payload.subject,
+      html: payload.html,
+    });
+  }
+}
