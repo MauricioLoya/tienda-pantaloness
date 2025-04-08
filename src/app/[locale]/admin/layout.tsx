76fcd6256fc5cd01fb6b2a07c660871133d6f906
AlexisLoya <alexloy117@gmail.com>
2025-04-07 20:33:19 -0600

    Add Setting model and update AdminLayout to retrieve settings

--- src/app/[locale]/admin/layout.tsx
+++ src/app/[locale]/admin/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { auth } from '../../auth';
 import { redirect } from 'next/navigation';
 import NavBar from '@/modules/admin-dashboard/layout/NavBar';
+import { SettingsRepository } from '@/modules/settings/definitions';
 
 type Props = {
   children: React.ReactNode;
@@ -9,7 +10,8 @@ type Props = {
 
 const AdminLayout: React.FC<Props> = async ({ children }) => {
   const session = await auth();
-
+  const logoUrl = await new SettingsRepository().get('logoUrl');
+  const storeName = await new SettingsRepository().get('storeName');
   if (!session?.user) {
     redirect('/api/auth/signin');
   }
@@ -18,7 +20,7 @@ const AdminLayout: React.FC<Props> = async ({ children }) => {
     <main>
       <div className='flex h-screen flex-col md:flex-row md:overflow-hidden bg-white'>
         <div className='w-full flex-none md:w-64'>
-          <NavBar />
+          <NavBar logoUrl={logoUrl} storeName={storeName} />
         </div>
         <section className='flex-grow md:p-4 md:overflow-y-auto md:px-12'>{children}</section>
       </div>
